f8349d0ed698fd4e205f60ba25fa0622b6d3eacc
jasontsang <jasontsang.icloud@me.com>
2017-04-21 16:08:03 +0800

    Initial Commit

--- boilerplate-sample/src/main/java/com/github/gnastnosaj/boilerplate/sample/MainActivity.java
+++ boilerplate-sample/src/main/java/com/github/gnastnosaj/boilerplate/sample/MainActivity.java
@@ -1,10 +1,31 @@
 package com.github.gnastnosaj.boilerplate.sample;
 
+import android.os.Bundle;
+
 import com.github.gnastnosaj.boilerplate.ui.activity.BaseActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by jasontsang on 4/21/17.
  */
 
 public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        createDynamicBox();
+
+        Observable.timer(3, TimeUnit.SECONDS).compose(this.<Long>bindToLifecycle()).subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(@NonNull Long aLong) throws Exception {
+                showDynamicBoxCustomView(DYNAMIC_BOX_MK_CLASSICSPINNER, MainActivity.this);
+            }
+        });
+    }
 }
