96bf0745926a9db1913025d9dcb7110fa68aa1e7
jasontsang <jasontsang.icloud@me.com>
2017-07-31 10:47:28 +0800

    Initial Commit

--- boilerplate/src/main/java/com/github/gnastnosaj/boilerplate/Boilerplate.java
+++ boilerplate/src/main/java/com/github/gnastnosaj/boilerplate/Boilerplate.java
@@ -29,13 +29,15 @@ public class Boilerplate {
     private static PatchManager patchManager;
 
     public static void initialize(Application application) {
-        initialize(application, null, null);
+        initialize(application, new Config());
     }
 
+    @Deprecated
     public static void initialize(Application application, @Nullable ImagePipelineConfig imagePipelineConfig) {
         initialize(application, imagePipelineConfig, null);
     }
 
+    @Deprecated
     public static void initialize(Application application, @Nullable ImagePipelineConfig imagePipelineConfig,
                                   @Nullable DraweeConfig draweeConfig) {
         instance = application;
@@ -61,6 +63,98 @@ public class Boilerplate {
 
         Fresco.initialize(application, imagePipelineConfig, draweeConfig);
         MVCHelper.setLoadViewFractory(new LoadViewFactory());
+    }
+
+    public static void initialize(Application application, Config config) {
+        instance = application;
+
+        DEBUG = application.getApplicationInfo() != null && (application.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+
+        if (config.log) {
+            CrashReportingTree.initialize(application);
+        }
+
+        try {
+            versionName = application.getPackageManager().getPackageInfo(application.getPackageName(), PackageManager.GET_CONFIGURATIONS).versionName;
+            versionCode = application.getPackageManager().getPackageInfo(application.getPackageName(), PackageManager.GET_CONFIGURATIONS).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            Timber.e(e, "Boilerplateh initialize Exception");
+        }
+
+        if (config.patch) {
+            patchManager = new PatchManager(application);
+            patchManager.init(versionName);
+            patchManager.loadPatch();
+        }
+
+        if (!DEBUG) {
+            Cockroach.install((Thread thread, Throwable throwable) -> Timber.wtf(throwable, "CockroachException", thread));
+        }
+
+        if (config.fresco) {
+            Fresco.initialize(application, config.imagePipelineConfig, config.draweeConfig);
+        }
+
+        if (config.mvc) {
+            MVCHelper.setLoadViewFractory(new LoadViewFactory());
+        }
+    }
+
+    public static class Config {
+        private boolean log = true;
+
+        private boolean fresco = true;
+        private ImagePipelineConfig imagePipelineConfig;
+        private DraweeConfig draweeConfig;
+
+        private boolean patch = true;
+
+        private boolean mvc = true;
+
+        private Config() {
+        }
+
+        public static class Builder {
+            private Config config;
+
+            public Builder() {
+                config = new Config();
+            }
+
+            public Builder log(boolean enable) {
+                config.log = enable;
+                return this;
+            }
+
+            public Builder fresco(boolean enable) {
+                config.fresco = enable;
+                return this;
+            }
+
+            public Builder setImagePipelineConfig(ImagePipelineConfig imagePipelineConfig) {
+                config.imagePipelineConfig = imagePipelineConfig;
+                return this;
+            }
+
+            public Builder setDraweeConfig(DraweeConfig draweeConfig) {
+                config.draweeConfig = draweeConfig;
+                return this;
+            }
+
+            public Builder patch(boolean enable) {
+                config.patch = enable;
+                return this;
+            }
+
+            public Builder mvc(boolean enable) {
+                config.mvc = enable;
+                return this;
+            }
+
+            public Config build() {
+                return config;
+            }
+        }
     }
 
     public static Application getInstance() {
