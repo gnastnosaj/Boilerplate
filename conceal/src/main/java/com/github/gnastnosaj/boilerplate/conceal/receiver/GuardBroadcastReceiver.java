d8ccb974f82fd584a2ca68a86feb2b6c35b69898
jasontsang <jasontsang.icloud@me.com>
2017-09-21 08:47:01 +0800

    conceal

--- conceal/src/main/java/com/github/gnastnosaj/boilerplate/conceal/receiver/GuardBroadcastReceiver.java
+++ conceal/src/main/java/com/github/gnastnosaj/boilerplate/conceal/receiver/GuardBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.github.gnastnosaj.boilerplate.conceal.service.ConcealService;
+import com.github.gnastnosaj.boilerplate.conceal.service.GuardService;
 
 /**
  * Created by jasontsang on 9/20/17.
@@ -14,5 +15,6 @@ public class GuardBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         context.startService(new Intent(context, ConcealService.class));
+        context.startService(new Intent(context, GuardService.class));
     }
 }
