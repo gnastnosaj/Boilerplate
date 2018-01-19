cee34af6b32e37a2db421856272328bdf37a7fbe
jasontsang <jasontsang.icloud@me.com>
2018-01-19 13:59:05 +0800

    cordova

---
+++ cordova.sample/src/main/java/com/github/gnastnosaj/boilerplate/cordova/sample/SampleActivity.java
@@ -0,0 +1,10 @@
+package com.github.gnastnosaj.boilerplate.cordova.sample;
+
+import com.github.gnastnosaj.boilerplate.ui.activity.BaseActivity;
+
+/**
+ * Created by jasontsang on 1/19/18.
+ */
+
+public class SampleActivity extends BaseActivity {
+}
