ee142bf8e88225a808e163bea5eefd250d2d11ba
jasontsang <jasontsang.icloud@me.com>
2018-01-29 15:48:06 +0800

    droidplugin

---
+++ plugin-sample/src/main/java/com/github/gnastnosaj/boilerplate/plugin/sample/PluginSampleActivity.java
@@ -0,0 +1,10 @@
+package com.github.gnastnosaj.boilerplate.plugin.sample;
+
+import com.github.gnastnosaj.boilerplate.ui.activity.BaseActivity;
+
+/**
+ * Created by jasontsang on 1/29/18.
+ */
+
+public class PluginSampleActivity extends BaseActivity {
+}
